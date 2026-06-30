7fbf4264a4cc81af6fdc817fe843220fb3786d30
Gyana Ranjan <gyanaranjansahoo174@gmail.com>
2026-06-30 14:16:25 +0530

    Added primsa schema

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrate: {
-    adapter: async () => {
-      return new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-    },
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL ?? '',
   },
 })
